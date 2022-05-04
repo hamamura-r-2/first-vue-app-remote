caff3c9d19fe080f5156c78259a8d58bd338db29
hamamura-r-2 <r.hamamura1227@gmail.com>
2022-05-04 21:15:59 +0900

    reaactive変数追加

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,15 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import Hello from './components/Hello.vue'
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + Vite" />
+  <Hello/>
+  <Hello/>
+  <Hello/>
 </template>
 
 <style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,23 @@
+<template>
+    <h2>Hello!!</h2>
+    <p>Ref Count:{{ count }}</p>
+    <p>Reactive Count:{{ state.count }}</p>
+    <div>
+        <button @click="addRefCount">Ref Count+</button>
+        <button @click="addReactiveCount">Reactive Count+</button>
+    </div>
+</template>
+
+<script setup>
+    import { ref, reactive } from 'vue';
+    const count = ref(0);
+    const state = reactive({
+        count: 1,
+    });
+    const addRefCount = () => {
+        count.value++;
+    };
+    const addReactiveCount = () => {
+        state.count++;
+    };
+</script>
